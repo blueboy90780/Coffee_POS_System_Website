1d53df9c6405e61655b571d5c9ea699a614aaf1c
Nguyen Tien Dung (David) <aisdavid9@gmail.com>
2022-11-24 20:45:10 +0700

    Followed through up to 1:07:31

--- Coffee_POS_System_ASP.NET_Core/Controllers/HelloWorldController.cs
+++ Coffee_POS_System_ASP.NET_Core/Controllers/HelloWorldController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Coffee_POS_System_ASP.NET_Core.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Coffee_POS_System_ASP.NET_Core.Controllers
 {
     public class HelloWorldController : Controller
     {
+        // Fields
+        private static List<DogViewModel> dogList = new();
+
         public IActionResult Index()
         {
-            return View(); //Will use the view that has the same name as this method inside the same folder as controller
+            return View(dogList); //Will use the view that has the same name as this method inside the same folder as controller
+        }
+
+        public IActionResult Create()
+        {
+            var dogVM = new DogViewModel();
+            return View(dogVM);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            //return View("Index");
+            dogList.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
         }
 
         public string Hello()
---
+++ Coffee_POS_System_ASP.NET_Core/Models/DogViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Coffee_POS_System_ASP.NET_Core.Models
+{
+    public class DogViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
